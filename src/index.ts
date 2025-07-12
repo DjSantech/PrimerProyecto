8075a60e04e69b1ee0019c17a152a08263ad663f
santiago guevara mendez <santiago.guevara1@utp.edu.co>
2025-07-12 16:35:30 -0500

    Router basico creado

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,9 @@
-import express from 'express'
+import server from './server'
 
-const app = express ();
 
-//Routing
-app.get("/" , (req, res) => {
-    res.send("Hola mundo en express")
-});
-
-app.get("/home" , (req, res) => {
-    res.send("Hola mundo en home")
-});
 
 const port = process.env.PORT || 4000
 
-app.listen (port, () =>{
+server.listen (port, () =>{
     console.log("Servidor funcionando en el puerto :", port)
 })
---
+++ src/router.ts
@@ -0,0 +1,11 @@
+import {Router} from 'express'
+
+const router = Router ()
+
+//Routing
+router.get("/" , (req, res) => {
+    res.send("Hola mundo en express")
+});
+
+
+export default router
