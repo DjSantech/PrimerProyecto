1ab73298d883c7fa326b931b9770508f64f222da
santiago guevara mendez <santiago.guevara1@utp.edu.co>
2025-07-23 12:25:16 -0500

    contraseñas hasheadas y encriptadas

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -1,10 +1,26 @@
+import type {Request,Response} from 'express' 
 import User from "../models/user" 
-export const createAccount = async (req, res) =>
+import { hashPassword } from '../utils/auth'
+export const createAccount = async (req : Request, res: Response) =>
 {   
-    const user = new User(req.body)
+
+    const {email,password} = req.body
+    const userExists = await User.findOne({email})
+
+    if(userExists){
+        const error = new Error("El usuario ya esta registrado")
+        return res.status(409).json({error: error.message})
+    } else {
+        console.log('No existe')
+    }
     
+
+    const user = new User(req.body)
+    const hash = await hashPassword(password)
+
+    console.log(hash)
 
     await user.save()
     
-    res.send('Registo creado')
+    res.status(201).send('Registo creado correctamente')
 }
