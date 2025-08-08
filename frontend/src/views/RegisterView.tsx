893740d1a9c08ff951c6ca1ab4a91d0479649333
santiago guevara mendez <santiago.guevara1@utp.edu.co>
2025-08-07 23:54:34 -0500

    agregando variables de entorno

--- frontend/src/views/RegisterView.tsx
+++ frontend/src/views/RegisterView.tsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom"
 import { useForm } from 'react-hook-form'
-import axios from 'axios'
+import axios, {isAxiosError} from 'axios'
 import ErrorMessage from "../components/ErrorMessage"
 import type { RegisterForm } from "../types"
 
@@ -25,10 +25,12 @@ export default function RegisterView() {
   const handleRegister = async (formData:RegisterForm) => {
     console.log("paso")
     try {
-      const response = await axios.post('http://localhost:4000/auth/register', formData)
-      console.log(response)
+      const {data } = await axios.post(` ${import.meta.env.VITE_API_URL}` , formData)
+      console.log(data)
     } catch (error) {
-      console.log(error)
+      if (isAxiosError(error) && error.response) {
+        console.log(error.response?.data.error)
+      }
     }
     
   }
