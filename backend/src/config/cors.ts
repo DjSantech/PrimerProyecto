ac9fcb275a92df9127d47b4980ee417f043afa38
santiago guevara mendez <santiago.guevara1@utp.edu.co>
2026-01-06 12:31:57 -0500

    JSON WebTocken agregado al proyecto

--- backend/src/config/cors.ts
+++ backend/src/config/cors.ts
@@ -3,7 +3,11 @@ import {CorsOptions} from 'cors'
 export const corsConfig: CorsOptions = {
     origin: function(origin, callback) {
         const whiteList = [process.env.FRONT_END_URL]
-        if (origin === whiteList[0] || !origin) {
+
+        if (process.argv [2] === '--api') {
+            whiteList.push(undefined)
+            }
+        if (whiteList.includes(origin)) {
             callback(null,true)
         } else {
             callback(new Error('Error de cors'))
